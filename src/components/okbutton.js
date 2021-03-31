f223a55ac98f94a4adfe562ad0ab033b3de0f1e3
Jukka Paulin <jukka.paulin@gmail.com>
2021-03-31 08:53:05 +0300

    OKButton has support for variable greet text (props)

--- src/components/okbutton.js
+++ src/components/okbutton.js
@@ -5,9 +5,15 @@ export class OKButton extends Component {
 
 	// A beautiful blue button that has simply text OK inside it
 	render() {
-	const labelText = 'OK';
-	return (
-			<div><button>{labelText}</button></div>
-	);
+		var labelText = undefined
+		// optional props, and a default value if no props found 
+		if (this.props.greet) {
+			labelText = this.props.greet
+		} else {
+			labelText = 'OK'
+		}
+		return (
+				<div><button>{labelText}</button></div>
+		);
 	}
 }
